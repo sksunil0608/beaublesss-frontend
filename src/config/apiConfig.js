8c036d702753668d9f7fb5e86f272b32b6bf20bb
sksunil0608 <sksunil0608@gmail.com>
2025-03-24 11:13:31 +0530

    v2.0.2

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     // baseURL:process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1',
-    // baseURL:'https://www.api.beaubless.com/api/v1',
-    baseURL:'http://localhost:5000/api/v1',
+    baseURL:'https://www.api.beaubless.com/api/v1',
+    // baseURL:'http://localhost:5000/api/v1',
 };
 export { apiConfig };
