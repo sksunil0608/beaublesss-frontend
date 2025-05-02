66d3275e920cc8fda4d486ef6f19af688987ee5b
sksunil0608 <sksunil0608@gmail.com>
2025-05-02 13:18:26 +0530

    contact image changed

--- src/components/otherPages/StoreLocations3.jsx
+++ src/components/otherPages/StoreLocations3.jsx
@@ -22,7 +22,7 @@ export default function StoreLocations3() {
                     allowFullScreen=""
                     loading="lazy"
                   /> */}
-                  <img src="/images/banner/contact.jpg" className=" p-5 rounded" style={{ border: 0,borderRadius:'10px',width: "100%", height: "100%" }}/>
+                  <img src="/images/banner/contact.jpg" className="rounded" style={{ border: 0,borderRadius:'10px',width: "100%", height: "100%" }}/>
                  
                 </div>
                  
